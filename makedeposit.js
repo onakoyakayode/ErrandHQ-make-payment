7ee20d3eb8a0217299ac6ccc3218274f44af3b45
Onakoya Kayode <onakoyakayode@gmail.com>
2022-03-22 19:29:23 +0100

    add files

---
+++ makedeposit.js
@@ -0,0 +1,12 @@
+let amt = document.getElementById('amount');
+let button1 = document.getElementById('btn1k');
+
+
+function btn1k() {
+    function fastPay() {
+        document.getElementById('amount').value = 5000;
+    }
+
+    fastPay()
+    
+}
